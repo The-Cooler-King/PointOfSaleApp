145e0061c3b06641c9a58c506689a7d3155f4585
ivanovakatya <katyaivanova0799@gmail.com>
2021-08-18 20:16:58 +0100

    Issue #3: allow the clerk to add multiple items and print list of items

--- checkout.py
+++ checkout.py
@@ -5,7 +5,10 @@ Created on Fri Aug 13 11:35:57 2021
 @author: vjham
 """
 
+purchased_items = []
+while True:
 
-item_id_input = input("Please enter item identifier: ")
+    item_id_input = input("Please enter item identifier: ")
+    purchased_items.append(item_id_input)
 
-print (item_id_input)
+    print (purchased_items)
