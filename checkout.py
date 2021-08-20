1bdbe514371d5657f65df321c37cc23ca34a2f38
ivanovakatya <katyaivanova0799@gmail.com>
2021-08-20 22:28:00 +0100

    Issue #5: Add grocery store name

--- checkout.py
+++ checkout.py
@@ -4,7 +4,12 @@ Created on Fri Aug 13 11:35:57 2021
 
 @author: vjham
 """
+"""
+After the clerk indicates there are no more items, the program should print a custom 
+receipt on the screen. At this point, the receipt should only include a grocery store 
+name of your choice.
 
+"""
 items = []
 while True:
 
@@ -13,4 +18,8 @@ while True:
         break
     items.append(item_id_input)
 
+
 print (items)
+grocery_store_name = "Shop 'N Bag"
+print (grocery_store_name)
+
