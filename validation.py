f150aa685a53dad86a8f6e025300ce8fa0baaace
The-Cooler-King <vham0927@gmail.com>
2021-08-20 15:05:42 -0700

    Issue #14: created class ProductService to contain our products list

--- validation.py
+++ validation.py
@@ -5,12 +5,12 @@ Created on Thu Aug 19 18:26:19 2021
 @author: vjham
 """
 
-from product_list import products
+from product_list import ProductService
 
 
 def validate_clerk_input(clerk_input):
     
-    list_of_valid_product_ids = [str(product['id']) for product in products]
+    list_of_valid_product_ids = [str(product['id']) for product in ProductService.products]
     
     if clerk_input in list_of_valid_product_ids or clerk_input == 'DONE':
         return clerk_input
