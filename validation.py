f836a8249e3094494a0dab6a5f21a0596066ee42
The-Cooler-King <vham0927@gmail.com>
2021-08-27 12:33:51 -0700

    Issue #14: made a list of valid product IDs an lru_cache

--- validation.py
+++ validation.py
@@ -5,11 +5,9 @@ Created on Thu Aug 19 18:26:19 2021
 @author: vjham
 """
 
-from product_list import ProductService
+from product_list import product_IDs
 
 
 def is_clerk_input_valid(clerk_input):
     
-    list_of_valid_product_ids = [str(product['id']) for product in ProductService.products]
-    
-    return clerk_input in list_of_valid_product_ids or clerk_input == 'DONE'
+    return clerk_input in product_IDs() or clerk_input == 'DONE'
